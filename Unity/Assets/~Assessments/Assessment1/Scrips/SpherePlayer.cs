3b878974d42bcc7fa7755e0757b4fad2a64e8a23
Lee Cselko <lee.cselko@gmail.com>
2018-06-07 21:22:46 +1000

    Finished Assignement !!

---
+++ Unity/Assets/~Assessments/Assessment1/Scrips/SpherePlayer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePlayer : MonoBehaviour
+{
+    public float speed = 6f;
+
+    public float jumpHeight = 3f;
+
+    private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void Move()
+    // Movement Script (WASD)
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(Vector3.back * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddForce(Vector3.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+    }
+    void Jump()
+    // Jump Script (Space)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+    // Item & Hazard Script
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            other.gameObject.SetActive(false);
+            Vector3 jump = new Vector3(0.0f, 30, 0.0f);
+            rigid.AddForce(jump * speed * Time.deltaTime);
+        }
+    }
+}
